59a6c338ce131d2a0169811593b778c3312ef5c3
JxCalmeiro <tonycalmeiro@gmail.com>
2025-01-09 17:10:23 -0500

    W1D2 finished demo

---
+++ Labs/Lab1/Lab1.py
@@ -0,0 +1,12 @@
+#Tony Calmeiro
+#SE126.04
+#Lab1
+#1-13-2025
+
+#PROGRAM PROMPT: This is a program that determines whether a meeting room is in violation of fire regulations regarding the maximum room capacity. 
+#The program will accept the maximum room capacity and the number of people attending the meeting. If the number of people is less than or equal to the maximum room capacity, 
+#the program announces that it is legal to hold the meeting and tells how many additional people may legally attend. 
+#If the number of people exceeds the maximum room capacity, the program announces that the meeting cannot be held as planned due to the fire regulation and tells how
+#many people must be excluded in order to meet the fire regulations. The user should be allowed to enter and check as many rooms as they would like without exiting the program.
+
+#VARIABLE DICTIONARY
