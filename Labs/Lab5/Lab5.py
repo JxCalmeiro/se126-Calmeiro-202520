7da0d46f929ec51139ae32b3035cb79c706c763c
JxCalmeiro <156533705+JxCalmeiro@users.noreply.github.com>
2025-02-13 20:49:14 -0500

    Update Lab5 2-13-25 20:49

--- Labs/Lab5/Lab5.py
+++ Labs/Lab5/Lab5.py
@@ -9,74 +9,57 @@
 import csv
 
 #VARIABLE DICTIONARY:
+#lib_number = []                list holds values for library numbers
+#title = []                     list holds values for book titles
+#author = []                    list holds values for book authors
+#genre = []                     list holds values for book genres
+#page_count = []                list holds values for book page counts
+#status = []                    list holds values for book status
+#found = []                     list holds result values from search 
+#ans                            allows user to enter search loop
+#search_type                    allows user to input search option (1-8)
+#search                         allows user to input search value
+#min                            sets lowest possible index
+#max                            sets highest index
+#mid                            sets middle index in sorted list
 
 #--FUNCTIONS-------------------------------------------------------
-#--search menu function--
-def search_menu():
-    choice = -1
-    valid_answer = False
+def display(x, records):
+    print(f"\n{'LIB#':6} {'TITLE':35} {'AUTHOR':20} {'GENRE':17} {'PAGE CT':12} {'STATUS':9}")
+    print("---------------------------------------------------------------------------------------------------------")
 
-    # validates user input to write data
-    while valid_answer == False:
-        answer = (input("\nWould you like to use the search function? (Y/N): "))
-        if answer.upper() == "Y" or answer.upper() == "N":
-            valid_answer = True
-        else:
-            print(f"Your input of '{answer}' was Invalid, Please try again.")
-    # prints search menu
-    if answer.upper() == "Y":
-        while choice != "8":
-            print("\nSearch Menu:")
-            print("1. Show All Titles")
-            print("2. Search by Title")
-            print("3. Search by Author")
-            print("4. Search by Genre")
-            print("5. Search by Library Number")
-            print("6. Show All Available")
-            print("7. Show All On Loan")
-            print("8. Exit")
+    if x != "x":
+        #printing one record
+        print(f"{lib_number[x]:6} {title[x]:35} {author[x]:20} {genre[x]:17} {page_count[x]:12} {status[x]:9}")
 
-            choice = input("\nEnter your choice: ")
-
-            # input #1 - Show All Titles
-            if choice == "1":
-                pass
-            # input #2 - Search by Title
-            elif choice == "2":
-                pass
-            # input #3 - Search by Author
-            elif choice == "3":
-                pass
-            # input #4 - Search by Genre 
-            elif choice == "4":
-                pass
-            # input #5 - Search by Library Number 
-            elif choice == "5":
-                pass
-            # input #6 - Show All Available 
-            elif choice == "6":
-                pass
-            # input #7 - Show All On Loan 
-            elif choice == "7":
-                pass
-            # input #8 - program exit
-            elif choice == "8":
-                print(f"\n~~ Exiting the program. ~~")
-            
-            else:
-                print(f"Your input of '{choice}' was Invalid, Please try again.")
+    elif found:
+        #printing multiples, based on length stored in 'foundList'
+        for i in range(0, records):
+            print(f"{lib_number[found[i]]:6} {title[found[i]]:35} {author[found[i]]:20} {genre[found[i]]:17} {page_count[found[i]]:12} {status[found[i]]:9}")
+    
     else:
-        print(f"\n~~ Exiting the Program ~~")
-    print(f"\nThank you for using the program.\nGoodbye!\n")
+        #printing full data, based on length stored in 'records'
+        for i in range(0, records):
+            print(f"{lib_number[i]:6} {title[i]:35} {author[i]:20} {genre[i]:17} {page_count[i]:12} {status[i]:9}")
+    
+    print("---------------------------------------------------------------------------------------------------------")
 
-#--displays data from search menu option--
-def multi_search(listFound):
-    print("\nSearch Results:")
-    print(f"\n{'LIB #':6} {'TITLE':35} {'AUTHOR':20} {'GENRE':17} {'PAGE CT':12} {'STATUS':9}")
-    print("---------------------------------------------------------------------------------------------------------")
-    for i in range(0, len(listFound)):
-        print(f"{lib_number[listFound[i]]:6} {title[listFound[i]]:35} {author[listFound[i]]:20} {genre[listFound[i]]:17} {page_count[listFound[i]]:12} {status[listFound[i]]:9}")
-    print("---------------------------------------------------------------------------------------------------------")
+def swap(i, listName):
+    temp = listName[i]
+    listName[i] = listName[i + 1]
+    listName[i + 1] = temp
+
+#sequential search for exact match
+def seqSearch1(search, listName):
+    for i in range(0, len(listName)):
+        if search.lower() == listName[i].lower():
+            found.append(i)
+
+#sequential search for key word match
+def seqSearch2(search, listName):
+    for i in range(0, len(listName)):
+        if search.lower() in listName[i].lower():
+            found.append(i)
             
 #--MAIN EXECUTING CODE---------------------------------------------
 
@@ -103,14 +86,148 @@ with open("text_files/book_list.csv") as csvfile:
         status.append(rec[5])               #status: Available/On Loan field
 #--disconnected to file--------------------------------------------
 
-# print field headers
-print(f"\n{'LIB #':6} {'TITLE':35} {'AUTHOR':20} {'GENRE':17} {'PAGE CT':12} {'STATUS':9}")
-print("---------------------------------------------------------------------------------------------------------")
+print("\n~~ Personal Library Catalog ~~")
+ans = input("\nWould you like to enter the search program? [y/n]: ").lower()
 
-# processing through lists for display
-for i in range(0, len(lib_number)):
-    print(f"{lib_number[i]:6} {title[i]:35} {author[i]:20} {genre[i]:17} {page_count[i]:12} {status[i]:9}")
-print("---------------------------------------------------------------------------------------------------------")
+#validity and user error trap loop
+while ans != "y" and ans != "n":
+    print("***INVALID ENTRY!***")
+    ans = input("\nWould you like to enter the search program? [y/n]: ").lower()
 
-# calls search_menu function to allow user to search data
-search_menu()
+#main searching loop
+while ans == "y":
+    found = [] #resets found list so each new menu/search it is empty
+
+    #--displays data from search menu option--
+    print("\nSearch Menu:")
+    print("1. Show All Titles")
+    print("2. Search by Title")
+    print("3. Search by Author")
+    print("4. Search by Genre")
+    print("5. Search by Library Number")
+    print("6. Show All Available")
+    print("7. Show All On Loan")
+    print("8. Exit")
+
+    search_type = input("\nEnter your search choice [1-8]: ")
+
+    #using 'not in' for user validity checks
+    if search_type not in ["1", "2", "3", "4", "5", "6", "7", "8"]:
+         print("***INVALID ENTRY!***\nPlease try again")
+
+    # input #1 - Show All Titles
+    elif search_type == "1":
+        #BUBBLE SORT --> sorting for ascending alpha order
+        for i in range(0, len(title) - 1):
+            for j in range(0, len(title) - 1):
+                if title[j] > title[j + 1]:
+                    temp = title[j]
+                    title[j] =  title[j + 1]
+                    title[j + 1] = temp
+                    #swap indices from parallel lists
+                    swap(j, lib_number)
+                    swap(j, author)
+                    swap(j, genre)
+                    swap(j, page_count)
+                    swap(j, status)
+        print("\nAll Titles:")
+        display("x", len(title))
+
+    # input #2 - Search by Title
+    elif search_type == "2":
+        search = input("\nEnter the Title or Title key word to search: ")
+        seqSearch2(search, title)
+
+        if not found:
+            print(f"\nSorry, we could not find your search for '{search}'. Please try again.")
+        else:
+            print(f"\nWe have found your search for '{search}', see details below:")
+            display("x", len(found))
+
+    # input #3 - Search by Author
+    elif search_type == "3":
+        search = input("\nEnter the Author to search: ")
+        seqSearch1(search, author)
+
+        if not found:
+            print(f"\nSorry, we could not find your search for '{search}'. Please try again.")
+        else:
+            print(f"\nWe have found your search for '{search}', see details below:")
+            display("x", len(found))
+
+    # input #4 - Search by Genre 
+    elif search_type == "4":
+        search = input("\nEnter the Genre to search: ")
+        seqSearch1(search, genre)
+
+        if not found:
+            print(f"\nSorry, we could not find your search for '{search}'. Please try again.")
+        else:
+            print(f"\nWe have found your search for '{search}', see details below:")
+            display("x", len(found))
+
+    # input #5 - Search by Library Number 
+    elif search_type == "5":
+        search = input("\nEnter the Library Number to search: ")
+        #BUBBLE SORT --> sorting for ascending numerical order
+        for i in range(0, len(lib_number) - 1):
+            for j in range(0, len(lib_number) - 1):
+                if lib_number[j] > lib_number[j + 1]:
+                    temp = lib_number[j]
+                    lib_number[j] =  lib_number[j + 1]
+                    lib_number[j + 1] = temp
+                    #swap indices from parallel lists
+                    swap(j, title)
+                    swap(j, author)
+                    swap(j, genre)
+                    swap(j, page_count)
+                    swap(j, status)
+
+        #BINARY SEARCH:
+        min = 0                         #lowest possible index
+        max = len(lib_number) - 1             #highest index
+        mid = int((min + max) / 2)      #middle index in sorted list
+
+        while min < max and search != lib_number[mid]:
+            #while above is true, list is not yet exhausted and we haven't found what we are looking for, so must go through another searching iteration!
+            if search < lib_number[mid]:
+                max = mid - 1
+            else: 
+                #search > name[mid]
+                min = mid + 1
+
+            mid = int((min + max) / 2)
+
+        if search.lower() == lib_number[mid].lower():
+            print(f"\nWe have found your search for '{search}', see details below:")
+            display(mid, len(lib_number))
+        else:
+            print(f"\nSorry, we could not find your search for '{search}'. Please try again.")
+
+    # input #6 - Show All Available 
+    elif search_type == "6":
+        seqSearch1("available", status)
+
+        if not found:
+            print(f"\nNo books are available at this time.")
+        else:
+            print(f"\nBooks that are available, see details below:")
+            display("x", len(found))
+
+    # input #7 - Show All On Loan 
+    elif search_type == "7":
+        seqSearch1("on loan", status)
+
+        if not found:
+            print(f"\nNo books are on loan at this time.")
+        else:
+            print(f"\nBooks that are on loan, see details below:")
+            display("x", len(found))
+
+    # input #8 - program exit
+    elif search_type == "8":
+        print(f"\n~~ Exiting the program. ~~")
+        ans = "N"
+
+#alert user that program is about to end
+print(f"\nThank you for using the program.\nGoodbye!\n")
