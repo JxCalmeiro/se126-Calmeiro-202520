2dbbebf6ed24841abc1566e6d6a007bc3a462af6
JxCalmeiro <156533705+JxCalmeiro@users.noreply.github.com>
2025-02-19 21:03:09 -0500

    updates 2-19-25 21:03

---
+++ CourseProject/disneyFilm_searchProgram.py
@@ -0,0 +1,249 @@
+#Tony Calmeiro
+#SE126.04
+#Course Project - Top 200 Disney Grossng Films Search Program
+#3-xx-25
+
+#PROGRAM PROMPT:
+
+#--IMPORTS---------------------------------------------------------
+import csv
+
+#--FUNCTIONS-------------------------------------------------------
+def display(x, records):
+    print(f"\n{'Rank':6} {'Year':6} {'Title':75} {'Production Studio':40} {'Genre':20} {'Worldwide Gross':15}")
+    print("-------------------------------------------------------------------------------------------------------------------------------------------------------------------------")
+
+    if x != "x":
+        #printing one record
+        print(f"{rank[x]:<6} {year[x]:<6} {title[x]:75} {prod_studio[x]:40} {genre[x]:20} ${int(gross[x]):15,}")
+
+    elif found:
+        #printing multiples, based on length stored in 'foundList'
+        for i in range(0, records):
+            print(f"{rank[found[i]]:<6} {year[found[i]]:<6} {title[found[i]]:75} {prod_studio[found[i]]:40} {genre[found[i]]:20} ${int(gross[found[i]]):15,}")
+    
+    else:
+        #printing full data, based on length stored in 'records'
+        for i in range(0, records):
+            print(f"{rank[i]:<6} {year[i]:<6} {title[i]:75} {prod_studio[i]:40} {genre[i]:20} ${int(gross[i]):15,}")
+    
+    print("-------------------------------------------------------------------------------------------------------------------------------------------------------------------------")
+
+#sorts data by Rank lowest to highest for search options
+def rankSort():
+    #BUBBLE SORT --> sorting for ascending numeric order
+    for i in range(0, len(rank) - 1):
+        for j in range(0, len(rank) - 1):
+            if (rank[j]) > (rank[j + 1]):
+                temp = rank[j]
+                rank[j] =  rank[j + 1]
+                rank[j + 1] = temp
+                #swap indices from parallel lists
+                swap(j, title)
+                swap(j, year)
+                swap(j, prod_studio)
+                swap(j, genre)
+                swap(j, gross)
+
+#swaps indicies when bubble sorting
+def swap(i, listName):
+    temp = listName[i]
+    listName[i] = listName[i + 1]
+    listName[i + 1] = temp
+
+#sequential search for exact match
+def seqSearch1(search, listName):
+    for i in range(0, len(listName)):
+        if search.lower() == listName[i].lower():
+            found.append(i)
+
+#sequential search for key word match
+def seqSearch2(search, listName):
+    for i in range(0, len(listName)):
+        if search.lower() in listName[i].lower():
+            found.append(i)
+
+#prints choice list for search option 6
+def studioList():
+    for i in range(0, len(studioChoices)):
+        print(f"{studioChoices[i]}")
+
+#prints choice list for search option 7
+def genreList():
+    for i in range(0, len(genreChoices)):
+        print(f"{genreChoices[i]}")
+#--created lists-----------------------------------------------------
+#Static Lists
+studioChoices = ["Disney-Pixar", "Disneytoon Studios", "Lucasfilm", "Marvel Studios", "Walt Disney Animation Studios", "Walt Disney Pictures"]
+genreChoices = ["Action", "Adventure", "Comedy", "Drama", "Musical", "Romantic Comedy", "Western"]
+# Appended Lists
+rank = []
+year = []
+title = []
+prod_studio = []
+genre = []
+gross = []
+
+#--MAIN EXECUTING CODE---------------------------------------------
+
+#--connected to file-----------------------------------------------
+with open("text_files/disney_movies.csv") as csvfile:
+    file = csv.reader(csvfile)
+
+    for rec in file:
+        rank.append(int(rec[0]))
+        year.append(int(rec[1]))
+        title.append(rec[2])
+        prod_studio.append(rec[3])
+        genre.append(rec[4])
+        gross.append(int(rec[5]))
+#--disconnected from file-------------------------------------------
+
+# main searching loop
+prog_title = "~~ Welcome to the Top 200 Grossing Disney Films Search Program! ~~"
+width = 115
+print()
+print(prog_title.center(width))
+print("------------------------------------------------------------------------------------------------------------------------")
+print("This program provides easy access to detailed information on Disney's highest-grossing films. Whether you're interested \nin exploring the full list of films, searching by specific criteria, or finding particular films by its rank, genre, \nor year, this tool offers various options to help you quickly find the information you're looking for.")
+print("------------------------------------------------------------------------------------------------------------------------")
+
+ans = input("\nWould you like to enter the search program? [y/n]: ").lower()
+
+#validity and user error trap loop
+while ans != "y" and ans != "n":
+    print("***INVALID ENTRY!***")
+    ans = input("\nWould you like to enter the search program? [y/n]: ").lower()
+
+#main searching loop
+while ans == "y":
+    found = [] #resets found list so each new menu/search it is empty
+
+    #--displays data from search menu option--
+    print("\nSearch Menu:")
+    print("1. Show All Films Alphabetically")
+    print("2. Search by Film Title")
+    print("3. Show All Films by Rank")
+    print("4. Search by Film Rank")
+    print("5. Search All Films by Year")
+    print("6. Search by Production Studio")
+    print("7. Search by Film Genre")
+    print("8. Exit")
+
+    search_type = input("\nEnter your search choice [1-8]: ")
+
+    #using 'not in' for user validity checks
+    if search_type not in ["1", "2", "3", "4", "5", "6", "7", "8"]:
+         print("***INVALID ENTRY!***\nPlease try again")
+
+    # input #1 - Show All Films Alphabetically
+    elif search_type == "1":
+        #BUBBLE SORT --> sorting for ascending alpha order
+        for i in range(0, len(title) - 1):
+            for j in range(0, len(title) - 1):
+                if title[j] > title[j + 1]:
+                    temp = title[j]
+                    title[j] =  title[j + 1]
+                    title[j + 1] = temp
+                    #swap indices from parallel lists
+                    swap(j, rank)
+                    swap(j, year)
+                    swap(j, prod_studio)
+                    swap(j, genre)
+                    swap(j, gross)
+        print("\nAll Films Alphabetically:")
+        display("x", len(title))
+    
+    # input #2 - Search by Film Title
+    elif search_type == "2":
+        search = input("\nEnter the Title or Title key word for the Film to search: ")
+        seqSearch2(search, title)
+
+        if not found:
+            print(f"\nSorry, we could not find your search for '{search}'. Please try again.")
+        else:
+            print(f"\nWe have found your search for '{search}', see details below:")
+            display("x", len(found))
+
+    # input #3 - Show All Films by Rank
+    elif search_type == "3":
+        rankSort()
+        print("\nAll Films by Rank:")
+        display("x", len(rank))
+
+    # input #4 - Search by Film Rank
+    elif search_type == "4":
+        search = int(input("\nEnter the Film Rank to search: "))
+        rankSort()
+        
+        #BINARY SEARCH:
+        min = 0                         #lowest possible index
+        max = len(rank) - 1             #highest index
+        mid = int((min + max) / 2)      #middle index in sorted list
+
+        while min < max and search != rank[mid]:
+            if search < rank[mid]:
+                max = mid - 1
+            else: 
+                #search > name[mid]
+                min = mid + 1
+
+            mid = int((min + max) / 2)
+
+        if search == rank[mid]:
+            print(f"\nWe have found your search for '{search}', see details below:")
+            display(mid, len(rank))
+        else:
+            print(f"\nSorry, we could not find your search for '{search}'. Please try again.")
+        
+    # input #5 - Search All Films by Year
+    elif search_type == "5":
+        search = int(input("\nEnter the Year to search: "))
+        rankSort()
+        for i in range(0, len(year)):
+            if search == year[i]:
+                found.append(i)
+
+        if not found:
+            print(f"\nSorry, we could not find your search for '{search}'. Please try again.")
+        else:
+            print(f"\nWe have found your search for '{search}', see details below:")
+            display("x", len(found))
+    
+    # input #6 - Search by Production Studio
+    elif search_type == "6":
+        print(f"\nChoices:")
+        studioList()
+        search = input("\nEnter the Film Production Studio to search: ")
+        rankSort()
+        seqSearch1(search, prod_studio)
+
+        if not found:
+            print(f"\nSorry, we could not find your search for '{search}'. Please try again.")
+        else:
+            print(f"\nWe have found your search for '{search}', see details below:")
+            display("x", len(found))
+
+
+    # input #7 - Search by Film Genre
+    elif search_type == "7":
+        print(f"\nChoices:")
+        genreList()
+        search = input("\nEnter the Film Genre to search: ")
+        rankSort()
+        seqSearch1(search, genre)
+
+        if not found:
+            print(f"\nSorry, we could not find your search for '{search}'. Please try again.")
+        else:
+            print(f"\nWe have found your search for '{search}', see details below:")
+            display("x", len(found))
+
+    
+    # input #8 - Program Exit
+    elif search_type == "8":
+        print(f"\n~~ Exiting the program. ~~")
+        ans = "N"
+
+#alert user that program is about to end
+print(f"\nThank you for using the program.\nGoodbye!\n")
---
+++ Labs/Lab6/Lab6.py
@@ -0,0 +1,83 @@
+#Tony Calmeiro
+#SE126.04
+#Lab 6
+#2-xx-25
+
+#PROGRAM PROMPT: This Program assigns seats in a small airplane with 7 rows (1-7) and 4 seats per row (A-D). It displays the seat layout, marking assigned seats with an 'X'. Users can reserve seats by typing their desired seat. If a seat is already taken, the program will notify the user and prompt them to choose another.
+
+#--FUNCTIONS-------------------------------------------------------
+def seatMap():
+    print(f"\n{'ROW':3}   {'A':3} {'B':3}   {'C':3} {'D':3}")
+    print("---------------------------------------------------------------")
+    for i in range(len(seatsOccupied)):
+        print(f"{i + 1:3}   {seatsOccupied[i][0]:3} {seatsOccupied[i][1]:3}   {seatsOccupied[i][2]:3} {seatsOccupied[i][3]:3}")
+    print("---------------------------------------------------------------\n")
+
+def continueReservation():
+    response = input("Do you want to reserve another seat? (Y/N): ").upper()
+    while response not in ["Y", "N"]:
+        print("***INVALID ENTRY!***\nPlease try again\n")
+        response = input("Do you want to reserve another seat? (Y/N): ").upper()
+    return response
+
+#--MAIN EXECUTING CODE---------------------------------------------
+
+#--INITIALIZE 2D LIST-----------------------------------------------
+seatsOccupied = [
+    ['A', 'B', 'C', 'D'],
+    ['A', 'B', 'C', 'D'],
+    ['A', 'B', 'C', 'D'],
+    ['A', 'B', 'C', 'D'],
+    ['A', 'B', 'C', 'D'],
+    ['A', 'B', 'C', 'D'],
+    ['A', 'B', 'C', 'D']
+]
+
+# prints seat map
+seatMap()
+
+#--INITIALIZE VARIABLES---------------------------------------------
+reserve_more = "Y"
+seats_filled = 0
+
+while reserve_more == "Y" and seats_filled < 28:
+    # gets user input for row
+    row = int(input("Enter which row you'd like to sit in [1-7]: "))
+    while row < 1 or row > 7:
+        print(f"\nYour input of '{row}' is Invalid! Please choose a row between 1 and 7.")
+        row = int(input("Enter which row you'd like to sit in [1-7]: "))
+
+    # gets user input for seat
+    seat = input("Enter which seat you'd like to sit in [A/B/C/D]: ").upper()
+    while seat not in ["A", "B", "C", "D"]:
+        print(f"\nYour input of '{seat}' is Ivalid! Please choose a seat from A, B, C, or D.")
+        seat = input("Enter which seat you'd like to sit in [A/B/C/D]: ").upper()
+
+    # finds seat index
+    if seat == "A":
+        seat_index = 0
+    elif seat == "B":
+        seat_index = 1
+    elif seat == "C":
+        seat_index = 2
+    else:
+        seat_index = 3
+
+    # checks if seat is already occupied
+    if seatsOccupied[row - 1][seat_index] == "X":
+        print(f"\nSorry, seat {row}{seat} is already occupied. Please choose another one.")
+    # assigns new seat and keeps count of occupied seats
+    else:
+        seatsOccupied[row - 1][seat_index] = "X"
+        seats_filled += 1
+        seatMap()
+
+    # asks user if they would like to countinue reserving seats
+    if seats_filled < 28:
+        reserve_more = continueReservation()
+
+# alerts user all seats are occupied and ends program
+if seats_filled == 28:
+    print("\nAll seats are occupied. No more reservations can be made.")
+
+print("\nThank you for using our program to assign your seats.\nGoodbye!\n")
