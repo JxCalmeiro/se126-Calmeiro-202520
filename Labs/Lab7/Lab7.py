dd6569d7398972c9db680ac40572b25cd930416c
JxCalmeiro <156533705+JxCalmeiro@users.noreply.github.com>
2025-02-28 08:45:45 -0500

    updates 2-28-25 8:45

--- Labs/Lab7/Lab7.py
+++ Labs/Lab7/Lab7.py
@@ -3,30 +3,46 @@
 #Lab 6
 #3-3-25
 
-#PROGRAM PROMPT:
+#PROGRAM PROMPT: This program lets users interact with a programming dictionary. Users can search for words, add new ones, view all words, exit via a menu, and saves updates to a new 'updated_words.csv' file
 
 #--IMPORTS---------------------------------------------------------
 import csv
 
 #--FUNCTIONS-------------------------------------------------------
+#--write data function--
 def writeFile():
     file = open("text_files/updated_words.csv", "w")
-    for key in pythonDict:
-        file.write(f"{key},{pythonDict[key]}\n")
+    for i in range(0, len(dict2Dlist)):
+        line = (f"{dict2Dlist[i][0]},{dict2Dlist[i][1]}")
+        if i < len(dict2Dlist) - 1:
+            file.write(line + "\n")  # Add newline for all but last entry
+        else:
+            file.write(line)  # No newline after the last entry
+    file.close()
 
+# --Swap function for dict2Dlist when sorted--
 def swap(i, listName):
     temp = listName[i]
     listName[i] = listName[i + 1]
     listName[i + 1] = temp
+
+# --Bubble Sort function for dict2Dlist--
+def bubble_sort(dict2Dlist):
+    for i in range(0, len(dict2Dlist) - 1):  # outer loop
+        for index in range(0, len(dict2Dlist) - 1 - i):  # inner loop
+            # Check if the current word (index 0 of the sublist) is greater than the next one
+            if dict2Dlist[index][0] > dict2Dlist[index + 1][0]:
+                # Swap if out of order
+                swap(index, dict2Dlist)
 #--MAIN EXECUTING CODE---------------------------------------------
 
-# empty dictionary
+# --empty dictionary--
 pythonDict = {}
 
-# empty 2D list
+# --empty 2D list--
 dict2Dlist = []
 
-# menu options list
+# --menu options list--
 menu_options = [
     "1. Show all words",
     "2. Search for a word",
@@ -43,23 +59,7 @@ with open("text_files/words.csv") as csvfile:
         dict2Dlist.append(rec)
 #--disconnected to file--------------------------------------------
 
-for i in range(len(dict2Dlist)):
-    for x in range(len(dict2Dlist[i])):
-        print(dict2Dlist[i][x])
-
-'''
-print(f"{'Word':23} {"Definition"}")
-print("-" * 185)
-for key in pythonDict:
-    #for every key stored to the yourCar dictionary
-    print(f"{key.upper():20}\t{pythonDict[key]}")
-print("-" * 185)
-'''
-
-
-
-
-# menu option loop
+# --main menu loop--
 user_choice = -1
 
 while user_choice != "4":
@@ -73,6 +73,7 @@ while user_choice != "4":
 
     user_choice = input("\nPlease choose an option: ")
 
+    # Shows all words
     if user_choice == "1":
         print("\nAll words in the dictionary:")
         print("-" * 28)
@@ -80,6 +81,7 @@ while user_choice != "4":
             print(key.upper())
         print("-" * 28)
 
+    # Search for a word
     elif user_choice == "2":
         search = input("Enter the word you want to search for: ")
         found = 0
@@ -96,25 +98,34 @@ while user_choice != "4":
             print(f"We could not find your search for '{search}' ")
 
 
+    # Add a word and definition
     elif user_choice == "3":
         word = input("Enter the word you want to add: ")
         definition = input(f"Enter the definition for '{word}': ")
         if word in pythonDict:
             print(f"'{word}' already exists in the dictionary.")
         else:
+             # Add to the dictionary
             pythonDict.update({word: definition})
+            # Add to the 2D list
+            dict2Dlist.append([word, definition])
             print(f"'{word}' has been added to the dictionary.")
 
+    # Sorts words alphabetically
     elif user_choice == "3.5":
         print("\nWords in alphabetical order:")
+        bubble_sort(dict2Dlist)
         print("-" * 28)
-        for word in sorted(pythonDict.keys()):
-            print(word.upper())
+        for entry in dict2Dlist:
+            print(f"{entry[0].upper():20}")
         print("-" * 28)
-            
+
+    # saves to new file 'updated_words.csv' and exits program       
     elif user_choice == "4":
             writeFile()
             print("\nChanges have been saved to 'updated_words.csv'\n~~ Exiting Program ~~")
+    
+    # displays invalid entry for menu option choices
     else:
         print("Invalid Choice. Please try again")
 
